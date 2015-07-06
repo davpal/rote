bab08ab859f7b09d015e6d244c4aba609c61a730
dave <paluchdd@gmail.com>
2015-07-06 14:36:24 +0200

    Add user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "You signed up successfully"
-      flash[:color]= "valid"
+      session[:user_id] = @user.id
+      redirect_to '/'
     else
-      flash[:notice] = "Form is invalid"
-      flash[:color]= "invalid"
+      redirect_to '/signup'
     end
-    render "new"
   end
 
   private
   def user_params
-
+    params.require(:user).permit(:email, :password)
   end
 end
